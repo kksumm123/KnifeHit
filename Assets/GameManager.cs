54ffc46161cd8547a5fecc7d1f6ba2d114fbe6d0
kksumm123 <kksumm123@gmail.com>
2021-10-15 19:33:59 +0900

    나이프 카운트 구현 및 이미지 연동

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,23 +2,48 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     GameObject knife;
+    Image baseKnifeIcon;
+    List<Image> knifeIcons = new List<Image>();
+    Sprite usableKnifeIcon;
+    Sprite usedKnifeIcon;
+    [SerializeField] int totalKnifeCount = 8;
+    [SerializeField] int usedKniftCount = 0;
     void Start()
     {
         knife = (GameObject)Resources.Load("Knife");
         knife.GetComponent<Knife>().enabled = false;
         knife.SetActive(false);
+        baseKnifeIcon = GameObject.Find("Canvas").transform.Find("KnifeCount/BaseIcon").GetComponent<Image>();
+        usableKnifeIcon = Resources.Load<Sprite>("UsableKnife");
+        usedKnifeIcon = Resources.Load<Sprite>("UsedKnife");
+
         CreateKnife();
+        InitKnifeIcons(totalKnifeCount);
+    }
+
+    void InitKnifeIcons(int totalKnifeCount)
+    {
+        baseKnifeIcon.gameObject.SetActive(true);
+
+        for (int i = 0; i < totalKnifeCount; i++)
+            knifeIcons.Add(Instantiate(baseKnifeIcon, baseKnifeIcon.transform.parent));
+
+        baseKnifeIcon.gameObject.SetActive(false);
+
+        knifeIcons.ForEach(x => x.sprite = usableKnifeIcon);
     }
 
     void Update()
     {
         if (Input.anyKeyDown && throwable)
         {
-           StartCoroutine(ThrowNewKnife());
+            if (usedKniftCount < totalKnifeCount)
+                StartCoroutine(ThrowNewKnife());
         }
     }
 
@@ -36,10 +61,16 @@ public class GameManager : MonoBehaviour
     {
         throwable = false;
         newKnife.GetComponent<Knife>().enabled = true;
+        IncreaseUsedKnifeCount();
 
         yield return new WaitForSeconds(throwDelay);
 
         CreateKnife();
     }
 
+    private void IncreaseUsedKnifeCount()
+    {
+        knifeIcons[usedKniftCount].sprite = usedKnifeIcon;
+        usedKniftCount++;
+    }
 }
