8e94ee59457064be606581a104e48b0e8ddb5745
kksumm123 <kksumm123@gmail.com>
2021-10-15 17:14:19 +0900

    GameManager.cs 완성

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -9,18 +9,23 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         knife = GameObject.Find("Knife");
+        knife.GetComponent<KnifeMove>().enabled = false;
         knife.SetActive(false);
     }
 
     void Update()
     {
         if (Input.anyKeyDown)
+        {
             CreateKnife();
+        }
     }
 
+    GameObject newKnife;
     void CreateKnife()
     {
-        var newKnife = Instantiate(knife);
-        newKnife.SetActive(true);    
+        newKnife = Instantiate(knife);
+        newKnife.GetComponent<KnifeMove>().enabled = true;
+        newKnife.SetActive(true);
     }
 }
