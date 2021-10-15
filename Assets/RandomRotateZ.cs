069a7c13e3411ebcbd49eadc7b13307451437756
kksumm123 <kksumm123@gmail.com>
2021-10-15 16:36:03 +0900

    1. 닷트윈설치 2. RandomRotateZ.cs 추가 및 작성 시작

---
+++ Assets/RandomRotateZ.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotateZ : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
